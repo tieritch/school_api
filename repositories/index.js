03cf8f92f5467c71a8273a4fab64eed5a60a1e06
tieritch <tieritch@gmail.com>
2025-07-06 09:48:05 +0200

    feat(repositories): add school_year.repository

--- repositories/index.js
+++ repositories/index.js
@@ -2,9 +2,11 @@ const userRepository=require('./user.repository');
 const roleRepository=require('./role.repository');
 const refreshTokenRepository=require('./refresh_token.repository');
 const permissionRepository=require('./permission_repository');
+const schoolYearRepository=require('./school_year.repository')
 module.exports={
     userRepository,
     roleRepository,
     refreshTokenRepository,
-    permissionRepository
+    permissionRepository,
+    schoolYearRepository
 }
---
+++ repositories/school_year.repository.js
@@ -0,0 +1,67 @@
+const {SchoolYear}=require('../models');
+const {db_connect}=require('../db/db_connect');
+const {Model}=require("objection");
+
+db_connect(process.env.NODE_ENV);
+
+/**
+ * @namespace
+ */
+const schoolYeaRepository={
+    /**
+     * This function returns list of all school years
+    * @function 
+    * @memberof schoolYearRepository
+    * @returns {Array}
+    */
+    findAll(){
+        return SchoolYear.query();
+    },
+
+   /**
+     * This function returns one school year based on informations provided by entity object
+     * @function
+     * @memberof schoolYearRepository
+     * @param {object} entity 
+     * @returns { object}
+     */
+    async findBy(entity){
+        const sch=await SchoolYear.query().where(entity).first();
+        return sch;
+    },
+
+    /** Create a  schoolYear based on informations provided by entity object and returns the created school year
+      * @function 
+      * @memberof schoolYearRepository
+      * @param {object} entity
+      * @returns {object}
+      */
+   create(entity){
+        return SchoolYear.query().insert(entity).returning('*');
+   },
+
+    /**
+     * Delete a school year based on informations provided by entity object and returns the deleted shool year
+     * @function 
+     * @memberof schoolYearRepository
+     * @param {object} entity 
+     * @returns {object}
+     */
+    remove(entity){
+        return ShoolYear.query().where(entity).delete().returning('*');
+    },
+
+    /**
+     * Update a school year with information based on condition
+     * @function
+     * @memberof schoolYearRepository
+     * @param {object} information
+     * @param {object} condition 
+     * @returns {object}
+     */
+    update(information,condition){
+        return SchoolYear.query().patch(information).where(condition).returning('*');
+             
+    }
+}
+module.exports=schoolYeaRepository;
