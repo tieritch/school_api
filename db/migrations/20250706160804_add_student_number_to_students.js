15c91c843cc747dd40b2d781f88bd2e15a2fda9b
tieritch <tieritch@gmail.com>
2025-07-06 18:17:35 +0200

    migration: add student_number column to students table

---
+++ db/migrations/20250706160804_add_student_number_to_students.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('students',(table)=>{
+    table.string('student_number').unique().notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.alterTable('students',(table)=>{
+    table.dropColumn('student_number');
+   })
+};
