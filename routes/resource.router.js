a12ec7e5486055ec16a0bb3ad6fbb75a3dcdb68a
tieritch <tieritch@gmail.com>
2025-07-08 15:56:09 +0200

    refactor(routes): remove unnecesseary endpoint from resource.router

---
+++ routes/resource.router.js
@@ -0,0 +1,45 @@
+const {resourceRepository}=require('../repositories');
+const {accessByToken,accessByRole}=require('../middlewares');
+const {query,body,param,validationResult}=require('express-validator');
+const express=require('express');
+const bcrypt=require('bcrypt');
+const router=express.Router();
+
+router
+
+/**
+ * @swagger
+ * tags:
+ *   name: Resources
+ *   description: Resource management endpoints. By Resources we mean database tables
+ */
+/**
+ * @swagger
+ * /resources:
+ *   get:
+ *     summary: Get all resources
+ *     tags: [Resources]
+ *     responses:
+ *       200:
+ *         description: A list of resources
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: Resource ID
+ *                   name:
+ *                     type: string
+ *                     description: Resource's name
+ * 
+ *                  
+ */
+.get('/resources',async(req,res)=>{
+    const resources=await resourceRepository.findAll();
+    res.json(resources);
+})
+module.exports=router;
