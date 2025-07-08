94494eb7aa4ebb9dcdcef1c5b65f641985a766cb
tieritch <tieritch@gmail.com>
2025-07-08 14:39:14 +0200

    refactor(repositories): remoove unused functions from resource.repository

--- repositories/resources.repository.js
+++ repositories/resources.repository.js
@@ -28,41 +28,5 @@ const resourceRepository={
         return resource;
     },
 
-    /** Create a resource based on informations provided by entity object and returns the created resource
-      * @function 
-      * @memberof resourceRepository
-      * @param {object} entity
-      * @returns {object}
-      */
-    create(entity){
-       return Resource.query().insert({entity}).returning('*');
-    },
-
-    
-    /**
-     * Update a reource with information based on condition
-     * @function
-     * @memberof resourceRepository
-     * @param {object} information
-     * @param {object} condition 
-     * @returns {object}
-     */
-    updateBy(information,condition){
-        return Resource.query().patch(information)
-        .where(condition).returning('*');
-    },
-
-      /**
-     * Delete a recource based on informations provided by entity object and returns the deleted resource
-     * @function 
-     * @memberof resourceRepository
-     * @param {object} entity 
-     * @returns {object}
-     */
-    async remove(entity){
-       // const resource=await Resource.relatedQuery('permissions').for( Resource.query().select('id').where(entity)).first();
-        await Resource.relatedQuery('permissions').for(entity.id).unrelate();    
-        return Resource.query().where(entity).delete().returning('*');        
-    },
 }
 module.exports=resourceRepository
