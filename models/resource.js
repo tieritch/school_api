5051fb009734493bdfb9c45a4a44979ff3651ea4
tieritch <tieritch@gmail.com>
2025-07-08 13:41:39 +0200

    feat(models): add permissions relation to recource

--- models/resource.js
+++ models/resource.js
@@ -5,6 +5,23 @@ module.exports=class Resource extends Model{
       static get tableName(){
         return 'resources';
       }
+
+      static get relationMappings(){        
+        return {
+          permissions:{
+            relation:Model.ManyToManyRelation,
+            modelClass:path.join(__dirname,'permission'),
+            join:{
+              from :'resources.id',
+              through:{
+                from:'roles_permissions_resources.resource_id',
+                to:'roles_permissions_resources.permission_id'
+              },
+              to:'permissions.id'
+            }
+          }
+        }
+      }
      
       $beforeInsert(){
        this.name=this.name.toLowerCase();
