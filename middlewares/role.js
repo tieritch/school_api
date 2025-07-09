fdb6d1af3bcfb3850cce6447046bbac0bc145ccd
tieritch <tieritch@gmail.com>
2025-07-09 17:21:14 +0200

    refactor(middleware): improve accessRole middleaware

--- middlewares/role.js
+++ middlewares/role.js
@@ -2,39 +2,33 @@ const { access } = require("../utils/gen_token")
 const {userRepository,permissionRepository,roleRepository}=require('.././repositories');
 const {Role,User}=require('.././models');
 
-function accessByRole(role,permission){
-   
-  return async(req,res,next)=>{
+function accessByRole(actions,resources){
+  /* actions and resources parameters are arrays
+     actions and ids : READ     5      resources and ids: courses      5
+                       CREATE   6                         students     6
+                       UPDATE   7                         grades       7
+                       DELETE   8                         school_years 8 
+                                        here will be more as I progress
+   */
+    return async (req, res, next) => {
+        const user = req.user;
+        const userFound=await userRepository.findBy({id:user.id});
+        const role=await userFound.$relatedQuery('roles').first();
         
-        const {username,password}=req.user;
-        const permissionFound=await permissionRepository.findBy({name:permission.trim().toUpperCase()});//['read','create','update','delete']   
-        const userFound=await userRepository.findBy({username,password});
-        
-        if(!permissionFound){
-            return res.status(401).json({error:'Permission does not exist'})
-        }
-        if(!userFound){
-            return res.status(401).json({error:'user does not exist'})
-        }
-       
-        const rolesOfUser=await userFound.$relatedQuery('roles');
-        if(!rolesOfUser.map(role=>role.name).includes(role)){
-            return res.status(401).json({error:` The user does not have ${role} role `})
-        }
-        
-        const roleFound=await roleRepository.findBy({name:role.trim().toLowerCase()});       
-        const permissionsOfRole=await roleFound.$relatedQuery('permissions');
-        
-        // permissions are for e.g:'create','read','update'
-        // admin role is supposed to have all permissions, that's why we check only for other roles
-        if(!permissionsOfRole.map(permission=>permission.name).includes(permission)
-          && role.trim().toLowerCase()!='admin'){ 
-            return res.status(401).json({
-                error:`${role} role does not have a ${permission} permission `
-            })
-        }    
-        next();
-    }
-
+        if(role.name!='admin'){
+            const dbUser = await User.query()
+            .findById(user.id)
+            .withGraphFetched('roles.[permissions,resources]');     
+            const allowed = dbUser.roles.some(role =>
+            actions.every(action=>role.permissions.map(permission=>permission.name).includes(action))
+              &&
+            resources.every(resource=>role.resources.map(resource=>resource.name).includes(resource)));                     
+          if (!allowed ) {
+           return res.status(403).json({ message: 'Access denied' });
+         }
+      }
+       return next();
+    };
+    
 }
 module.exports=accessByRole;
