f89563460f223b7756bdff71600c6db276625394
tieritch <tieritch@gmail.com>
2025-07-11 10:18:18 +0200

    feat(migrations): add UNIQUE constraint to resources.name

---
+++ db/migrations/20250711080800_unique_resource_name.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('resources',(table)=>{
+    table.unique('name');
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropUnique('name')
+};
