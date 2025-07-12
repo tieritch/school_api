b4149ee5a09d9baa8541f08e1efa29b2157b27d3
tieritch <tieritch@gmail.com>
2025-07-12 16:00:41 +0200

    feat(db/migrations): add migration for student_sequence schema

---
+++ db/migrations/20250712080921_student_sequences.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('student_sequences',(table)=>{
+    table.string('year',4).primary();
+    table.bigInteger('last_seq').notNullable();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('student_sequences');
+};
