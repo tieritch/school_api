9ef5ddea14d3b19600b95403d37cd1958019badc
tieritch <tieritch@gmail.com>
2025-07-13 09:00:35 +0200

    feat(db/migrations): update courses table by adding max_score column

---
+++ db/migrations/20250712155537_add_max_score_column.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+   
+    return knex.schema.alterTable('courses',(table)=>{
+        table.integer('max_score').notNullable();
+        table.integer('by');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+   return knex.schema.alterTable('courses',(table)=>{
+    table.dropColumn('max_score');
+    table.dropColumn('by');
+   })
+};
