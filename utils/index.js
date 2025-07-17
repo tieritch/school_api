80ed2d9f2875295d82c72a804e01e51112f22271
tieritch <tieritch@gmail.com>
2025-07-17 09:40:58 +0200

    feat(utils/wrapper): add asyncHandler wrapper to simplfy async route error handling

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,8 @@
 const createToken=require('./gen_token');
 const generateHexStudentNumber=require('./gen_number');
+const asyncHandler=require('./wrapper');
 module.exports={
     createToken,
-    generateHexStudentNumber
+    generateHexStudentNumber,
+    asyncHandler,
 }
