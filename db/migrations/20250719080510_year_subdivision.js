734c50acd85c56d2e602f52a9b990466450bb85d
tieritch <tieritch@gmail.com>
2025-07-19 12:15:01 +0200

    feat(db/migration): create migration for academic_subdivions table

---
+++ db/migrations/20250719080510_year_subdivision.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+   
+   return knex.schema.createTable('academic_subdivisions', (table)=>{
+    table.increments('id');
+    table.string('name').notNullable();
+    table.integer('number_of_periods').notNullable();
+    table.integer('school_year_id').references('id').inTable('school_years').notNullable();
+    table.timestamps(true,true);
+   })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('academic_subdivision')
+};
